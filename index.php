25ae5b7c39a2391f52db9b2bc368bd9a0be80125
ugarix <ugarix@outlook.com>
2019-06-03 15:32:09 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,20 @@
+<!doctype html>
+
+<html>
+
+<head>
+
+<title>Ansible Essentials</title>
+
+</head>
+
+<body>
+
+<h1>php ugarix/ansible !</h1>
+
+<p> App backend <?=gethostname()?> </p>
+
+<img src="http://fillmurray.com/408/287" />
+
+</body>
+
